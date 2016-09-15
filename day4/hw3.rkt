83a3e3bde84aa52361b5b7c8e5414b7299dbedd6
Andrew Pan <andrew.pan@students.olin.edu>
2016-09-14 23:07:41 -0400

    turning in hw3.rkt

--- day4/hw3.rkt
+++ day4/hw3.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Andrew Pan
 ;;;
 ;;; Check one:
 ;;; [ ] I completed this assignment without assistance or external resources.
@@ -17,9 +17,15 @@
 ;;
 ;; If the key is not found in the list, `assq` returns `#f`.
 
+(define (assq key ass)
+	(if (null? ass)
+		#f
+		(if (eq? (first (first ass)) key)
+			(cons key (second (first ass)))
+			(assq key (rest ass)))))
 
-
-
+(display (assq 'b '((a "apple") (b "banana") (c "canteloupe")))) (newline) ;; -> banana
+(display (assq 'd '((a "apple") (b "banana") (c "canteloupe")))) (newline) ;; -> #f
 
 ;;;;;;;;;;;
 ;; 2. lookup-list
@@ -28,3 +34,33 @@
 ;;
 ;; Add the `lookup-list` argument to your hw2 evaluator (or ours, from the solution set).
 ;; `(evaluate 'foo lookup-list)` should return whatever `'foo` is associated with in `lookup-list`.
+
+(define (lookup key ass)
+	(if (null? ass)
+		#f
+		(if (eq? (first (first ass)) key)
+			(second (first ass))
+			(lookup key (rest ass)))))
+
+(define (calculate x)
+  (cond [(integer? x) x]
+  		[(equal? (first x) 'ADD) (+ (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'SUB) (- (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'MUL) (* (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'DIV) (/ (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'GT) (> (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'LT) (< (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'GE) (>= (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'LE) (<= (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'EQ) (= (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'NEQ) (= not ((calculate (first (rest x))) (calculate (first (rest (rest x))))))]
+  		[(equal? (first x) 'ANDD) (and (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'ORR) (or (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'NOTT) (not (calculate (first (rest x))) (calculate (first (rest (rest x)))))]
+  		[(equal? (first x) 'IPH) (if (calculate (first (rest x)))
+  									(calculate (first (rest (rest x))))
+  									(calculate (first (rest (rest (rest x))))))]
+  		[(equal? (first x) 'LOOK) (lookup (second x) (third x))]))
+
+(display (calculate '(LOOK c ((a "apple") (b "banana") (c "canteloupe"))))) (newline) ;; -> canteloupe
+(display (calculate '(ADD 3 (LOOK b ((a 1) (b 2) (c 3)))))) (newline) ;; -> 5
